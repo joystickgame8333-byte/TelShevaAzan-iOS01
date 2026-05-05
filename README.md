5e70ef971547f504e57edc17dc339d608051db7e
Omar <omar@example.local>
2026-05-05 14:13:46 +0300

    Use legacy widget theme codepath

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TelShevaAzan iOS Prototype
 
-Current app version: `0.4.44 (70)`
+Current app version: `0.4.45 (71)`
 
 مشروع iPhone أولي لتطبيق أذان تل السبع.
 
--- TelShevaAzan/AppInfo.swift
+++ TelShevaAzan/AppInfo.swift
@@ -1,5 +1,5 @@
 enum AppInfo {
-    static let version = "0.4.44"
-    static let build = "70"
+    static let version = "0.4.45"
+    static let build = "71"
     static let displayVersion = "v\(version) (\(build))"
 }
