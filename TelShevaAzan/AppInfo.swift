001e65aef86ed975663e4e07b348a8a22ff487f2
Omar <omar@example.local>
2026-05-06 14:03:09 +0300

    Match watch companion id for Sideloadly

--- TelShevaAzan/AppInfo.swift
+++ TelShevaAzan/AppInfo.swift
@@ -1,5 +1,5 @@
 enum AppInfo {
-    static let version = "0.5.0"
-    static let build = "81"
+    static let version = "0.5.1"
+    static let build = "82"
     static let displayVersion = "v\(version) (\(build))"
 }
