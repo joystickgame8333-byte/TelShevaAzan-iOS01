e1815a45ae3c270c89ad94afff815a276d9d4dfe
Omar <omar@example.local>
2026-05-24 21:55:50 +0300

    Apply new widget design on stable registration

--- TelShevaAzan/AppInfo.swift
+++ TelShevaAzan/AppInfo.swift
@@ -1,5 +1,5 @@
 enum AppInfo {
-    static let version = "0.6.41"
-    static let build = "132"
+    static let version = "0.6.44"
+    static let build = "135"
     static let displayVersion = "v\(version) (\(build))"
 }
