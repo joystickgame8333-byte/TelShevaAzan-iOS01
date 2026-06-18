a88c4aa38fde3e93724767106c4ebc47c7b180d9
Omar <omar@example.local>
2026-06-18 16:38:35 +0300

    Use single configurable widget registration

--- TelShevaAzanWidget/TelShevaAzanWidget.swift
+++ TelShevaAzanWidget/TelShevaAzanWidget.swift
@@ -1599,17 +1599,6 @@ struct TelShevaAzanWidgetBundle: WidgetBundle {
 #if WIDGET_V3
         if #available(iOSApplicationExtension 17.0, *) {
             SalatiConfigurableWidget()
-        } else {
-            TelShevaAzanWidget()
-            TelShevaAzanScheduleWidget()
-            TelShevaAzanCountdownWidget()
-            if #available(iOSApplicationExtension 16.0, *) {
-                SalatiPrayerTimeLockCircleWidget()
-                SalatiIqamaMinutesLockCircleWidget()
-                SalatiIqamaTimeLockCircleWidget()
-                SalatiNextCountdownLockCircleWidget()
-                SalatiSunriseLockCircleWidget()
-            }
         }
         if #available(iOSApplicationExtension 16.1, *) {
             PrayerLiveActivityWidget()
