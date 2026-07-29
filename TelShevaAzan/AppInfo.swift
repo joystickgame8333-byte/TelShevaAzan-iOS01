d8a7ed773cf08180a38b3797a05a60ff55739899
omalasam309-ai <omalasam309@gmail.com>
2026-07-29 23:01:37 +0300

    Prepare version 0.6.83

--- TelShevaAzan/AppInfo.swift
+++ TelShevaAzan/AppInfo.swift
@@ -1,5 +1,5 @@
 enum AppInfo {
-    static let version = "0.6.82"
-    static let build = "175"
+    static let version = "0.6.83"
+    static let build = "176"
     static let displayVersion = "v\(version) (\(build))"
 }
