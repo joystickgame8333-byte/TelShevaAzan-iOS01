44fe58148d51246173991314b25804bb3ee79ae8
omalasam309-ai <omalasam309@gmail.com>
2026-08-11 00:14:09 +0300

    Fix native Apple Watch installation packaging

--- TelShevaAzan/AppInfo.swift
+++ TelShevaAzan/AppInfo.swift
@@ -1,5 +1,5 @@
 enum AppInfo {
-    static let version = "0.6.97"
-    static let build = "191"
+    static let version = "0.6.98"
+    static let build = "192"
     static let displayVersion = "v\(version) (\(build))"
 }
